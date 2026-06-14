cec70de4e46e1548a4856d7cfee85951a069b405
umar689 <umark064588@gmail.com>
2026-06-14 18:30:03 +0530

    Update mongoose.js
    Updated MongoDB connection string to an empty value.

--- basic_social_media_app/config/mongoose.js
+++ basic_social_media_app/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialDB');
+mongoose.connect('');
 
 const db = mongoose.connection;
 
